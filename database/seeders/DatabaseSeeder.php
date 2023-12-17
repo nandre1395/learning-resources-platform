a696304392c9272290ca73c32b68dd9799147078
AndreaB113 <116130652+nandre1395@users.noreply.github.com>
2023-12-16 20:03:25 -0500

    Inicio del proyexto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        category::factory()->count(5)->create();
     }
 }
